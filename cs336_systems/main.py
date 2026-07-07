cf9b734f7733ffc55963e182d0cf1a15a6298f0e
PrathamAditya <prathamadityasalhotra@gmail.com>
2026-07-07 10:10:09 +0530

    Implementation of benchmarking script.

--- cs336_systems/main.py
+++ cs336_systems/main.py
@@ -1,5 +1,5 @@
-import torch
-print(torch.randint(0, 10000, (4, 512)).shape)
-# from benchmarking_script import method
-
-# method(120, 4, 2, 90, None,warmup_steps=5,steps=100)
+# import torch
+# print(torch.randint(0, 10000, (4, 512)).shape)
+from benchmarking_script import method
+w_type = "fbo"
+method(120, 4, 2, 90, None,warmup_steps=1,steps=10,which_type=w_type)
